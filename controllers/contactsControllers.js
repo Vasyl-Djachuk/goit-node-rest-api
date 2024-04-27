ba0c02d8a3649563d4cdacdcd224623223270bd4
Vasyl Diachuk <djachukvasja@gmail.com>
2024-04-27 19:20:39 +0300

    done

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -1,11 +1,42 @@
 import contactsService from "../services/contactsServices.js";
+import HttpError from "../helpers/HttpError.js";
 
-export const getAllContacts = (req, res) => {};
+export const getAllContacts = async (req, res) => {
+  const contacts = await contactsService.listContacts();
+  res.status(200).send(contacts);
+};
 
-export const getOneContact = (req, res) => {};
+export const getOneContact = async (req, res, next) => {
+  const { id } = req.params;
+  const contact = await contactsService.getContactById(id);
+  if (contact) {
+    res.status(200).json(contact);
+  } else {
+    next(HttpError(404));
+  }
+};
 
-export const deleteContact = (req, res) => {};
+export const deleteContact = async (req, res, next) => {
+  const { id } = req.params;
+  const removeContact = await contactsService.removeContact(id);
+  if (removeContact) {
+    res.status(200).json(removeContact);
+  } else {
+    next(HttpError(404));
+  }
+};
 
-export const createContact = (req, res) => {};
+export const createContact = async (req, res) => {
+  const addedContact = await contactsService.addContact(req.body);
+  res.status(201).json(addedContact);
+};
 
-export const updateContact = (req, res) => {};
+export const updateContact = async (req, res, next) => {
+  const { id } = req.params;
+  const editedContact = await contactsService.updateContact({
+    ...req.body,
+    id,
+  });
+  if (editedContact === null) return next(HttpError(404));
+  res.status(200).json(editedContact);
+};
--- schemas/contactsSchemas.js
+++ schemas/contactsSchemas.js
@@ -1,9 +1,25 @@
 import Joi from "joi";
 
 export const createContactSchema = Joi.object({
-
-})
+  name: Joi.string().min(3).max(20).required(),
+  email: Joi.string()
+    .email({
+      minDomainSegments: 2,
+      tlds: { allow: ["com", "net"] },
+    })
+    .required(),
+  phone: Joi.string().required(),
+});
 
 export const updateContactSchema = Joi.object({
-
+  name: Joi.string().min(3).max(20),
+  email: Joi.string().email({
+    minDomainSegments: 2,
+    tlds: { allow: ["com", "net"] },
+  }),
+  phone: Joi.string(),
 })
+  .or("phone", "email", "name")
+  .messages({
+    "object.missing": "Body must have at least one field",
+  });
